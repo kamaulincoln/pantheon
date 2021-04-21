ae0483f6ba80fd5f1e4ec7e416921dbadc3bca8e
Zhengxu Xia <kevinxzx95@gmail.com>
2021-04-20 19:34:09 -0500

    Update driver scripts.

--- drivers/run.sh
+++ drivers/run.sh
@@ -2,30 +2,33 @@
 
 set -e
 
-# src/experiments/test.py local --schemes "cubic vivace aurora" -t 60 \
-#     --prepend-mm-cmds "mm-delay 50" \
-#     --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args='packets=149'"
-
-# src/experiments/test.py local --schemes "aurora cubic"  \
-#     --uplink-trace tests/30mbps_data.trace --downlink-trace tests/30mbps_ack.trace \
-#     --prepend-mm-cmds "mm-delay 50 mm-loss uplink 0.01" \
-#     --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args='packets=10'" \
-#     --data-dir exp
-# src/analysis/analyze.py --data-dir exp
 
 # model_path=models/udr_4_dims_correct_recv_rate/range0/model_to_serve_step_1504800
-model_path=models/udr_4_dims_correct_recv_rate/range0/model_step_1504800.ckpt
-src/experiments/test.py local --schemes "aurora" -t 40 \
+# model_path=models/udr_4_dims_correct_recv_rate/range0/model_step_1504800.ckpt
+# model_path=models/model_for_real_net/from_scratch_seed_20/model_step_619200.ckpt
+# model_path=models/model_for_real_net/from_scratch_seed_20/model_step_619200.ckpt
+model_path=models/udr_7_dims_fix_val_reward/range0_queue50/model_step_252000.ckpt
+# save_dir=test_constant_bw
+# save_dir=test_train_env
+# save_dir=test_rtt_9_new_model
+save_dir=test_cc
+src/experiments/test.py local --schemes "aurora" -t 11 \
     --uplink-trace tests/traces/2mbps --downlink-trace tests/traces/12mbps \
-    --prepend-mm-cmds "mm-delay 50 mm-loss uplink 0.0" \
-    --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args='packets=10'" \
-    --data-dir test_aurora_new \
-    --model-path  ${model_path} \
-    --aurora-save-dir test_aurora_new \
+    --prepend-mm-cmds "mm-delay 50 mm-loss uplink 0.00" \
+    --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args='packets=50'" \
+    --data-dir ${save_dir} \
+    --model-path ${model_path} \
+    --aurora-save-dir ${save_dir} \
     --pyprogram /home/zxxia/.virtualenvs/aurora/bin/python
 # --downlink-queue=droptail --downlink-queue-args='packets=10'
-    # --model-path ./range0/model_to_serve_step_1504800 \
-python src/analysis/plot.py --data-dir test_aurora_new/ --schemes "aurora"
+#     --model-path ./range0/model_to_serve_step_1504800 \
+python src/analysis/plot.py --data-dir ${save_dir} --schemes "aurora"
+python drivers/plot/plot_time_series.py \
+    --log-file ${save_dir}/aurora_datalink_run1.log ${save_dir}/cubic_datalink_run1.log \
+    --save-dir ${save_dir}
+python drivers/plot/plot_log.py \
+    --log-file ${save_dir}/aurora_emulation_log.csv \
+    --save-dir ${save_dir}
 # python /home/zxxia/pantheon/src/analysis/report.py --data-dir exp/ --schemes "aurora cubic"
 pkill -9 iperf
 
--- drivers/run_remote.sh
+++ drivers/run_remote.sh
@@ -2,23 +2,34 @@
 
 set -e
 
-target_host="cc@192.5.87.193"
+target_host="cc@192.5.86.228"
 target_patheon_dir=/home/cc/Projects/pantheon
 
-model_path=models/udr_4_dims_correct_recv_rate/range0/model_step_1504800.ckpt
-src/experiments/test.py remote --schemes "aurora" -t 40 \
-    --data-dir test_aurora_new \
-    --model-path  ${model_path} \
-    --aurora-save-dir test_aurora_new \
-    --pyprogram /home/zxxia/.virtualenvs/aurora/bin/python \
-    ${target_host}:${target_patheon_dir}
-# --downlink-queue=droptail --downlink-queue-args='packets=10'
-    # --model-path ./range0/model_to_serve_step_1504800 \
-python src/analysis/plot.py --data-dir test_aurora_new/ --schemes "aurora"
-# python /home/zxxia/pantheon/src/analysis/report.py --data-dir exp/ --schemes "aurora cubic"
-pkill -9 iperf
+# model_path=models/udr_4_dims_correct_recv_rate/range0/model_step_1504800.ckpt
+# model_path=models/model_for_real_net/from_scratch_seed_20/model_step_864000.ckpt
+model_path=models/model_for_real_net/from_scratch_seed_20/model_step_619200.ckpt
+# model_path=models/model_for_real_net/from_scratch_seed_20/model_step_1065600.ckpt
+#
+# 2 3 4 5 6 7 8 9
+for idx in 0 1 ; do
+    # src/experiments/test.py remote --schemes "aurora cubic" -t 30 \
+    #     --data-dir test_remote2/run${idx} \
+    #     --model-path  ${model_path} \
+    #     --aurora-save-dir test_remote2/run${idx} \
+    #     --pyprogram /home/zxxia/.virtualenvs/aurora/bin/python \
+    #     --ntp-addr 129.6.15.29 \
+    #     ${target_host}:${target_patheon_dir}
+    # python src/analysis/plot.py --data-dir test_remote1/run${idx}/ --schemes "aurora cubic "
+    # # python /home/zxxia/pantheon/src/analysis/report.py --data-dir exp/ --schemes "aurora cubic"
+    python drivers/plot/plot_time_series.py --log-file test_remote2/run${idx}/aurora_datalink_run1.log --save-dir test_remote2/run${idx}
+    python drivers/plot/plot_log.py --log-file test_remote2/run${idx}/aurora_emulation_log.csv --save-dir test_remote2/run${idx}
+    python drivers/plot/plot_time_series.py --log-file test_remote2/run${idx}/cubic_datalink_run1.log --save-dir test_remote2/run${idx}
+    pkill -9 iperf
+done
 
-
+# sudo tc qdisc add dev wlp2s0 root tbf rate 3mbit burst 1540
+# sudo tc qdisc del dev wlp2s0 root
+# latency 50ms
 
 # src/experiments/test.py local --schemes "cubic" -t 15\
 #     --uplink-trace cubic_datalink_run2_mahimahi.log --downlink-trace tests/12mbps_ack.trace \
