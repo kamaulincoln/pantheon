972bc78e4556049ebd327b509dedf7ce38eda3ee
Zhengxu Xia <kevinxzx95@gmail.com>
2021-04-26 15:52:11 -0500

    Save run_remote

--- drivers/run_remote.sh
+++ drivers/run_remote.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-target_host="cc@192.5.86.228"
+# target_host="cc@192.5.86.228"
+target_host="cc@129.114.109.236"
 target_patheon_dir=/home/cc/Projects/pantheon
 
 # model_path=models/udr_4_dims_correct_recv_rate/range0/model_step_1504800.ckpt
@@ -11,21 +12,33 @@ model_path=models/model_for_real_net/from_scratch_seed_20/model_step_619200.ckpt
 # model_path=models/model_for_real_net/from_scratch_seed_20/model_step_1065600.ckpt
 #
 # 2 3 4 5 6 7 8 9
-for idx in 0 1 ; do
-    # src/experiments/test.py remote --schemes "aurora cubic" -t 30 \
-    #     --data-dir test_remote2/run${idx} \
+model_path=models/udr_7_dims_fix_val_reward/range0_queue50/model_step_252000.ckpt
+# save_dir=test_const_sending_rate_no_mahimahi
+save_dir=test_aurora_in_mahimahi
+# 0
+# 2
+
+for idx in   0 1 2 3 4; do
+        # --ntp-addr 129.6.15.30 \
+    # src/experiments/test.py remote --schemes "aurora" -t 11 \
+    #     --data-dir ${save_dir}/run${idx} \
     #     --model-path  ${model_path} \
-    #     --aurora-save-dir test_remote2/run${idx} \
+    #     --aurora-save-dir ${save_dir}/run${idx} \
     #     --pyprogram /home/zxxia/.virtualenvs/aurora/bin/python \
-    #     --ntp-addr 129.6.15.29 \
     #     ${target_host}:${target_patheon_dir}
-    # python src/analysis/plot.py --data-dir test_remote1/run${idx}/ --schemes "aurora cubic "
-    # # python /home/zxxia/pantheon/src/analysis/report.py --data-dir exp/ --schemes "aurora cubic"
-    python drivers/plot/plot_time_series.py --log-file test_remote2/run${idx}/aurora_datalink_run1.log --save-dir test_remote2/run${idx}
-    python drivers/plot/plot_log.py --log-file test_remote2/run${idx}/aurora_emulation_log.csv --save-dir test_remote2/run${idx}
-    python drivers/plot/plot_time_series.py --log-file test_remote2/run${idx}/cubic_datalink_run1.log --save-dir test_remote2/run${idx}
-    pkill -9 iperf
+    src/experiments/test.py remote --schemes "cubic" -t 6 \
+        --data-dir ${save_dir}/run${idx} \
+        --model-path  ${model_path} \
+        --aurora-save-dir ${save_dir}/run${idx} \
+        --pyprogram /home/zxxia/.virtualenvs/aurora/bin/python \
+        ${target_host}:${target_patheon_dir}
+    # python drivers/plot/plot_time_series.py --log-file ${save_dir}/run${idx}/aurora_datalink_run1.log --save-dir ${save_dir}/run${idx}
+    # python drivers/plot/plot_log.py --log-file ${save_dir}/run${idx}/aurora_emulation_log.csv --save-dir ${save_dir}/run${idx}
+    python drivers/plot/plot_time_series.py --log-file ${save_dir}/run${idx}/cubic_datalink_run1.log --save-dir ${save_dir}/run${idx}
+    # pkill -9 iperf
 done
+    # python src/analysis/plot.py --data-dir test_remote1/run${idx}/ --schemes "aurora cubic "
+    # python /home/zxxia/pantheon/src/analysis/report.py --data-dir exp/ --schemes "aurora cubic"
 
 # sudo tc qdisc add dev wlp2s0 root tbf rate 3mbit burst 1540
 # sudo tc qdisc del dev wlp2s0 root
