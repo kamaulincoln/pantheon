c941c734dacc48dee6baac6098f1fdbdc0ea3afe
Zhengxu Xia <kevinxzx95@gmail.com>
2021-07-21 23:09:11 -0500

    Improve mahimahi trace conversion logic.

--- drivers/flow.py
+++ drivers/flow.py
@@ -1,16 +1,28 @@
 import os
-import sys
 import numpy as np
 from src.analysis.tunnel_graph import TunnelGraph
-from drivers.utils import pcc_aurora_reward, write_json_file
+from drivers.utils import pcc_aurora_reward
 
 
 class Flow():
+    """One way flow of the network connection."""
+
     def __init__(self, log_path, ms_per_bin=500):
         self.tunnel_graph = TunnelGraph(log_path, ms_per_bin=ms_per_bin)
         self.tunnel_graph.parse_tunnel_log()
         self.cc = str(os.path.basename(log_path).split("_")[0])
         self.ms_per_bin = ms_per_bin
+
+        if (1 not in self.tunnel_graph.egress_t or
+            1 not in self.tunnel_graph.egress_tput or
+            1 not in self.tunnel_graph.avg_egress or
+            1 not in self.tunnel_graph.ingress_t or
+            1 not in self.tunnel_graph.ingress_tput or
+            1 not in self.tunnel_graph.avg_ingress or
+            1 not in self.tunnel_graph.delays_t or
+            1 not in self.tunnel_graph.delays or
+                1 not in self.tunnel_graph.loss_rate):
+            raise RuntimeError("Invalid format {}".format(log_path))
 
     @property
     def link_capacity_timestamps(self):
@@ -70,13 +82,15 @@ class Flow():
 
 
 class Connection:
+    """Connection contains an uplink flow and downlink flow."""
+
     def __init__(self, trace_file):
         self.datalink = Flow(trace_file)
         self.acklink = Flow(trace_file.replace('datalink', 'acklink'))
 
     @property
     def rtt(self):
-        return (np.min(self.datalink.one_way_delay) + np.mean(self.acklink.one_way_delay)) / 2
+        return (np.min(self.datalink.one_way_delay) + np.min(self.acklink.one_way_delay)) / 2
 
     def reward(self, avg_bw=None):
         if avg_bw is None:
@@ -92,3 +106,36 @@ class Connection:
              np.mean(self.acklink.one_way_delay)) / 1000,
             self.datalink.loss_rate)
         return reward
+
+    def to_mahimahi_trace(self):
+        """Convert trace to Mahimahi format."""
+        timestamps = self.datalink.throughput_timestamps
+        bandwidths = self.datalink.throughput
+
+        ms_series = []
+        assert len(timestamps) == len(bandwidths)
+        ms_t = 0
+        for ts, next_ts, bw in zip(timestamps[0:-1], timestamps[1:], bandwidths[0:-1]):
+            pkt_per_ms = bw * 1e6 / 8 / 1500 / 1000
+
+            ms_cnt = 0
+            pkt_cnt = 0
+            while True:
+                ms_cnt += 1
+                ms_t += 1
+                to_send = np.floor((ms_cnt * pkt_per_ms) - pkt_cnt)
+                for _ in range(int(to_send)):
+                    ms_series.append(ms_t)
+
+                pkt_cnt += to_send
+
+                if ms_cnt >= (next_ts - ts) * 1000:
+                    break
+        return ms_series
+
+    def dump_mahimahi_trace(self, filename):
+        """Save trace in mahimahi format to the specified filename."""
+        ms_series = self.to_mahimahi_trace()
+        with open(filename, 'w', 1) as f:
+            for ms in ms_series:
+                f.write(str(ms) + '\n')
--- drivers/mahimahi_trace_converter.py
+++ drivers/mahimahi_trace_converter.py
@@ -1,13 +1,16 @@
 import glob
 import os
+from drivers.flow import Connection
 
 # 2020-02-17T21-29-AWS-California-1-to-Stanford-5-runs
 # trace_path = '/data/traces/2020-02-17T21-29-AWS-California-1-to-Stanford-5-runs/cubic_datalink_run1.log'
 traces_dir = '/home/zxxia/Projects/pantheon/data'
-save_dir = "/home/zxxia/Projects/pantheon/data/pantheon_mahimahi_traces_new"
+# save_dir = "/home/zxxia/Projects/pantheon/data/pantheon_mahimahi_traces_new"
+save_dir = "/home/zxxia/Projects/pantheon/data/pantheon_mahimahi_traces"
+save_dir = "/home/zxxia/Projects/pantheon/data/pantheon_mahimahi_traces_new_logic"
 
 scenarios = ['cellular'] #, 'ethernet', 'wireless']
-target_cc = 'bbr'
+target_cc_list = ['bbr', 'cubic', 'vegas']
 
 def convert(trace_path, save_dir):
     filename = os.path.basename(trace_path)
@@ -37,13 +40,24 @@ for scenario in scenarios:
             continue
         out_link_dir = os.path.join(save_dir, scenario, link_name)
         print(out_link_dir)
-        target_logs = glob.glob(os.path.join(link_dir, "{}_datalink*.log".format(target_cc)))
-        # if len(target_logs) == 0:
-        #     target_logs = glob.glob(os.path.join(link_dir, "default_tcp_acklink*.log"))
+        for target_cc in target_cc_list:
+            target_logs = glob.glob(os.path.join(
+                link_dir, "{}_datalink*.log".format(target_cc)))
+            # if len(target_logs) == 0:
+            #     target_logs = glob.glob(os.path.join(link_dir, "default_tcp_acklink*.log"))
 
-        for trace_path in target_logs:
-            print(trace_path)
-            if not os.path.exists(out_link_dir):
-                os.makedirs(out_link_dir)
-            print(out_link_dir)
-            convert(trace_path, out_link_dir)
+            for trace_path in target_logs:
+                print(trace_path)
+                if not os.path.exists(out_link_dir):
+                    os.makedirs(out_link_dir)
+                print(out_link_dir)
+                # convert(trace_path, out_link_dir)
+                try:
+                    connection = Connection(trace_path)
+                except RuntimeError:
+                    continue
+
+                filename = os.path.basename(trace_path)
+                # save_dir = os.path.dirname(trace_path)
+                out_trace_name = os.path.join(save_dir, out_link_dir, filename)
+                connection.dump_mahimahi_trace(out_trace_name)
